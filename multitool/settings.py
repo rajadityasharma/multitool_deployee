835b91215f9c932988da93f8f2e5149e32e64cdd
rajadityasharma <adityasharmamama@gmail.com>
2025-07-03 10:10:31 +0530

    update setting.py

--- multitool/settings.py
+++ multitool/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hsw_)7ftret5vk%bux6f!9+p*z&gml)q67lk6^m2!d-&lj@-@g'
-
+# SECRET_KEY = 'django-insecure-hsw_)7ftret5vk%bux6f!9+p*z&gml)q67lk6^m2!d-&lj@-@g'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
